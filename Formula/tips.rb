90d46b2372eb4a2fac6dd742ab95e060d1211994
theFranco2 <96888894+theFranco2@users.noreply.github.com>
2025-11-27 12:52:21 -0500

    Update URL and SHA256 for Tips formula to v1.0.2

--- Formula/tips.rb
+++ Formula/tips.rb
@@ -1,8 +1,8 @@
 class Tips < Formula
   desc "Tips gives you fast explanations for commands. Simply."
   homepage "https://github.com/theFranco2/homebrew-tips"
-  url "https://github.com/theFranco2/homebrew-tips/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "bd9ac84a7947b865a40f80a75279bdd1198982672dd79de1938e090db461aebb"
+  url "https://github.com/theFranco2/homebrew-tips/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   license "BSD-2-Clause"
 
   depends_on "cmake" => :build
